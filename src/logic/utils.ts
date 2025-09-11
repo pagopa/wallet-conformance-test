1c29da145366fe45118b5d9977b9d447f6f242af
giovanni-riela <giovanni.riela@itauros.com>
2025-09-11 11:53:56 +0200

    chore: refactor and add logs to metadata discovery test

--- src/logic/utils.ts
+++ src/logic/utils.ts
@@ -3,6 +3,8 @@ import { parse } from "ini";
 import { BinaryLike, createHash, randomBytes } from "node:crypto";
 import { readFileSync } from "node:fs";
 
+import { Logger } from "ts-log";
+
 import { Config } from "@/types";
 
 import { verifyJwt } from ".";
@@ -25,4 +27,54 @@ export function loadConfig(fileName: string): Config {
   const textConfig = readFileSync(fileName, "utf-8");
 
   return parse(textConfig) as Config;
+}
+
+export async function fetchWithRetries(
+	url: string | URL | Request,
+	network: Config["network"],
+	log: Logger,
+	message: string = `Fetching data from ${url}`,
+) {
+	let retries = network.max_retries;
+
+	log.info(message);
+
+	let res: Response;
+	try {
+		res = await fetch(
+			url,
+			{
+				method: "GET",
+				headers: {
+					"User-Agent": network.user_agent,
+				},
+				signal: AbortSignal.timeout(network.timeout * 1000)
+			}
+		);
+	} catch (err: any) {
+		if (err.name === "TimeoutError") {
+			log.error(`Request timed out: aborting`);
+			throw err;
+		}
+
+		if (retries-- <= 0) {
+			log.error(`Request failed with no retries left: aborting`);
+			throw err;
+		}
+
+		log.warn(`Request failed: ${retries} retries left`);
+		res = await fetchWithRetries(
+			url, 
+			{
+				...network,
+				max_retries: retries
+			},
+			log,
+			message
+		);
+	}
+
+	log.info(`Request completed wih status ${res.status}`);
+	
+	return res;
 }
