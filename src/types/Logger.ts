65329c12018759cc98a0e1d6cee452a01325d512
giovanni-riela <giovanni.riela@itauros.com>
2025-09-19 18:15:39 +0200

    feat: add custom logger

--- src/types/Logger.ts
+++ src/types/Logger.ts
@@ -1,34 +1,14 @@
-import { Logger } from "ts-log";
+import { ConsolaInstance } from "consola"
 
-export class Log implements Logger {
-	trace(message?: any, ...optionalParams: any[]) {
-		const output = optionalParams.length > 0 
-			? `${message} ${JSON.stringify(optionalParams, null, 4)}`
-			: message
-		console.trace("TRACE:", output);
+export type SetLogOptionsCallback = (
+	this: ConsolaInstance,
+	options: {
+		level?: string,
+		format?: string,
+		path?: string
 	}
-	debug(message?: any, ...optionalParams: any[]) {
-		const output = optionalParams.length > 0 
-			? `${message} ${JSON.stringify(optionalParams, null, 4)}`
-			: message
-		console.debug("DEBUG:", output);
-	}
-	info(message?: any, ...optionalParams: any[]) {
-		const output = optionalParams.length > 0 
-			? `${message} ${JSON.stringify(optionalParams, null, 4)}`
-			: message
-		console.info("INFO:", output);
-	}
-	warn(message?: any, ...optionalParams: any[]) {
-		const output = optionalParams.length > 0 
-			? `${message} ${JSON.stringify(optionalParams, null, 4)}`
-			: message
-		console.warn("WARN:", output);
-	}
-	error(message?: any, ...optionalParams: any[]) {
-		const output = optionalParams.length > 0 
-			? `${message} ${JSON.stringify(optionalParams, null, 4)}`
-			: message
-		console.error("ERROR:", output);
-	}
+) => void;
+
+export type Logger = ConsolaInstance & {
+	setLogOptions: SetLogOptionsCallback
 }
