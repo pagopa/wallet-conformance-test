e3bc529a311b13e7a22e97bd591644fbd96c5808
giovanni-riela <giovanni.riela@itauros.com>
2025-10-24 15:14:54 +0200

    chore: lint

--- src/types/Config.ts
+++ src/types/Config.ts
@@ -1,6 +1,15 @@
 import { z } from "zod";
 
 export const configSchema = z.object({
+  issuance: z.object({
+    credentials: z.object({
+      types: z.record(z.string(), z.array(z.string())),
+    }),
+    url: z.string(),
+  }),
+  trust: z.object({
+    ca_cert_path: z.string(),
+  }),
   wallet: z.object({
     backup_storage_path: z.coerce.string(),
     credentials_storage_path: z.coerce.string(),
@@ -9,15 +18,6 @@ export const configSchema = z.object({
     wallet_name: z.coerce.string(),
     wallet_provider_base_url: z.coerce.string(),
   }),
-	trust: z.looseObject({
-		ca_cert_path: z.string()
-	}),
-	issuance: z.object({
-		url: z.string(),
-		credentials: z.object({
-			types: z.record(z.string(), z.array(z.string()))
-		})
-	})
 });
 
 /**
--- tests/credentials.unit.spec.ts
+++ tests/credentials.unit.spec.ts
@@ -1,31 +1,38 @@
-import { test } from "vitest";
-
 import { parse } from "ini";
-import { loadCredentials } from "@/functions";
 import { readFileSync } from "node:fs";
+import { expect, test } from "vitest";
+
+import { loadCredentials } from "@/functions";
 import { Config } from "@/types";
+import { ValidationError } from "@pagopa/io-wallet-utils";
 
-test("Mocked Credentials Validation", () => {
-        const textConfig = readFileSync("config.ini", "utf-8");
-        const issuerKey = JSON.parse(
-            readFileSync("tests/data/backup/issuer_jwk.pub", "utf-8")
-        );
-        const config = parse(textConfig) as Config;
-        const types: string[] = []
+test("Mocked Credentials Validation", async () => {
+  const textConfig = readFileSync("config.ini", "utf-8");
+  const config = parse(textConfig) as Config;
+  const types: string[] = [];
 
-        for (const type in config.issuance.credentials.types) {
-            const issuerHasType = config.issuance.credentials.types[type]?.
-                find(t => t === config.issuance.url)
+  for (const type in config.issuance.credentials.types) {
+    const issuerHasType = config.issuance.credentials.types[type]?.find(
+      (t) => t === config.issuance.url,
+    );
 
-            if (issuerHasType)
-                types.push(type)
+    if (issuerHasType) types.push(type);
+  }
+
+  try {
+    await loadCredentials(
+      "tests/data/credentials",
+      types,
+    );
+  } catch (e) {
+    if (e instanceof ValidationError) {
+        console.error("Schema validation failed");
+        expect
+          .soft(
+            e.message.replace(": ", ":\n\t").replace(/,([A-Za-z])/g, "\n\t$1"),
+          )
+          .toBeNull();
         }
-
-        loadCredentials(
-            "tests/data/credentials",
-            types,
-            issuerKey,
-            "tests/data/certs/cert.pem"
-        );
-    }
-);
+    else throw e;
+  }
+});
