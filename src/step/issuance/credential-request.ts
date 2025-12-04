1ca8675a64226ede2956a66ac178a27af7f58902
giovanni-riela <giovanni.riela@itauros.com>
2025-12-04 14:45:37 +0100

    feat: fetch credential response

--- src/step/issuance/credential-request.ts
+++ src/step/issuance/credential-request.ts
@@ -1,41 +1,41 @@
 import {
-  createPushedAuthorizationRequest,
-  CreatePushedAuthorizationRequestOptions,
-  fetchPushedAuthorizationResponse,
-  fetchPushedAuthorizationResponseOptions,
-  PushedAuthorizationResponse,
-} from "@pagopa/io-wallet-oauth2";
-
-import {
   createCredentialRequest,
-  CredentialRequestOptions
+  CredentialRequestOptions,
+  CredentialResponse,
+  fetchCredentialResponse,
+  FetchCredentialResponseOptions,
 } from "@pagopa/io-wallet-oid4vci";
 
-import { partialCallbacks, signJwtCallback } from "@/logic";
+import { signJwtCallback } from "@/logic";
 import { StepFlow, StepResult } from "@/step";
 import { AttestationResponse } from "@/types";
 
-export type CredentialRequestExecuteResponse =
-  CredentialResponse;
+export type CredentialRequestExecuteResponse = CredentialResponse;
+
+export type CredentialRequestResponse = StepResult & {
+  response?: CredentialResponse;
+};
 
 export interface CredentialRequestStepOptions {
+  accessToken: string;
+
+  baseUrl: string;
+
   /**
    * Client ID of the OAuth2 Client,
    * if not provided, the client ID will be loaded from the wallet attestation public key kid
    */
   clientId: string;
 
-  /**
-   * DPoP JWT used to authenticate the client,
-   * if not provided, the DPoP will be created using the wallet attestation
-   */
-  popAttestation: string;
+  credentialIdentifier: string;
 
   /**
-   * Pushed Authorization Request Endpoint URL,
+   * Credential Request Endpoint URL,
    * if not provided, the endpoint will be loaded from the issuer metadata
    */
-  credentialRequestEndpoint?: string;
+  credentialRequestEndpoint: string;
+  dpop: string;
+  nonce: string;
 
   /**
    * Wallet Attestation used to authenticate the client,
@@ -44,21 +44,17 @@ export interface CredentialRequestStepOptions {
   walletAttestation: Omit<AttestationResponse, "created">;
 }
 
-export type CredentialRequestResponse = StepResult & {
-  response?: CredentialResponse;
-};
-
 export class CredentialRequestDefaultStep extends StepFlow {
   tag = "CREDENTIAL_REQUEST";
 
   async run(
     options: CredentialRequestStepOptions,
   ): Promise<CredentialRequestResponse> {
-	const log = this.log.withTag(this.tag);
+    const log = this.log.withTag(this.tag);
 
-	log.info(`Starting Credential Request Step`);
+    log.info(`Starting Credential Request Step`);
 
-	const { unitKey } = options.walletAttestation;
+    const { unitKey } = options.walletAttestation;
 
     return this.execute<CredentialRequestExecuteResponse>(async () => {
       const createCredentialRequestOptions: CredentialRequestOptions = {
@@ -66,16 +62,32 @@ export class CredentialRequestDefaultStep extends StepFlow {
           signJwt: signJwtCallback([unitKey.privateKey]),
         },
         clientId: options.clientId,
-        credential_identifier: "",
-        issuerIdentifier: "",
-        nonce: "",
+        credential_identifier: options.credentialIdentifier,
+        issuerIdentifier: options.baseUrl,
+        nonce: options.nonce,
         signer: {
           alg: "ES256",
           method: "jwk",
           publicJwk: unitKey.publicKey,
         },
       };
-      await createCredentialRequest(createCredentialRequestOptions)
+      const credentialRequest = await createCredentialRequest(
+        createCredentialRequestOptions,
+      );
+
+      log.debug(
+        `Fetching Credential response from ${options.credentialRequestEndpoint}`,
+      );
+      const fetchCredentialResponseOptions: FetchCredentialResponseOptions = {
+        accessToken: options.accessToken,
+        callbacks: {
+          fetch,
+        },
+        credentialEndpoint: options.credentialRequestEndpoint,
+        credentialRequest,
+        dPoP: options.dpop,
+      };
+      return await fetchCredentialResponse(fetchCredentialResponseOptions);
     });
   }
 }
