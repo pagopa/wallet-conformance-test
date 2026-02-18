3e58382da8fb11752d44e509e0b82449f3449818
giovanni-riela <giovanni.riela@itauros.com>
2026-02-18 11:04:20 +0100

    fix: add validity info to mock mdl credential

--- src/functions/mock-credentials.ts
+++ src/functions/mock-credentials.ts
@@ -46,6 +46,8 @@ export async function createMockMdlMdoc(
     .useDigestAlgorithm("SHA-256")
     .addValidityInfo({
       signed: new Date(),
+      validFrom: new Date(),
+      validUntil: expiration,
     })
     .addDeviceKeyInfo({ deviceKey })
     .sign({
