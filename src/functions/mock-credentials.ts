885f5259fc58171c4d24a3d0243506e799334453
Giovanni Riela <giovanni.riela@itauros.com>
2026-02-24 12:05:43 +0100

    feat: add x5c to mocked credential (#64)

--- src/functions/mock-credentials.ts
+++ src/functions/mock-credentials.ts
@@ -178,6 +178,12 @@ export async function createMockSdJwt(
 
   const vct = "urn:eudi:pid:1";
   const vctIntegrity = await generateSRIHash(vct);
+  const certificate = await loadCertificate(
+    backupPath,
+    "issuer_cert",
+    issuer.keyPair,
+    "CN=test_issuer",
+  );
 
   const credential = await sdjwt.issue(
     {
@@ -201,6 +207,7 @@ export async function createMockSdJwt(
         kid: issuer.keyPair.privateKey.kid,
         trust_chain: issuer.trust_chain,
         typ: "dc+sd-jwt",
+        x5c: [certificate],
       },
     },
   );
