211560117cc0c42dd53b4da7609f2be50b4c7eaf
giovanni-riela <giovanni.riela@itauros.com>
2026-04-28 01:42:05 +0200

    feat: prepare for unhappy flows

--- src/types/presentation-orchestrator-context.ts
+++ src/types/presentation-orchestrator-context.ts
@@ -2,10 +2,21 @@ import type { AuthorizationRequestStepResponse } from "@/step/presentation";
 import type { FetchMetadataVpStepResponse } from "@/step/presentation";
 import type { RedirectUriStepResponse } from "@/step/presentation";
 
+import { AttestationResponse } from "./attestation-response";
+import { CredentialWithKey } from "./credential";
+
 export interface PresentationFlowResponse {
-  authorizationRequestResult?: AuthorizationRequestStepResponse;
+  authorizationRequestResponse?: AuthorizationRequestStepResponse;
   error?: Error;
-  fetchMetadataResult?: FetchMetadataVpStepResponse;
-  redirectUriResult?: RedirectUriStepResponse;
+  fetchMetadataResponse?: FetchMetadataVpStepResponse;
+  redirectUriResponse?: RedirectUriStepResponse;
   success: boolean;
+}
+
+export interface RunThroughAuthorizeContext {
+  authorizationRequestResponse: AuthorizationRequestStepResponse;
+  credentials: CredentialWithKey[];
+  fetchMetadataResponse: FetchMetadataVpStepResponse;
+  verifierMetadata: any;
+  walletAttestationResponse: AttestationResponse;
 }
