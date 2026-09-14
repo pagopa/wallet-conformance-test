afa5d6d2e586ba77e52e53a825bec998bf2126e9
sebastianopriscan <sebastianroberto.opriscan@gmail.com>
2026-09-14 19:00:44 +0200

    fix: [WLEO-1378] Remove RP's metadata enc and sig key validation from QEAA authorize step

--- src/step/issuance/authorize-step.ts
+++ src/step/issuance/authorize-step.ts
@@ -1,6 +1,7 @@
 import {
   AuthorizationResponse,
   sendAuthorizationResponseAndExtractCode,
+  SendAuthorizationResponseAndExtractCodeOptions,
 } from "@pagopa/io-wallet-oid4vci";
 import {
   createAuthorizationResponse,
@@ -160,22 +161,6 @@ export class AuthorizeDefaultStep extends StepFlow {
       );
     }
 
-    const rpEncKey = options.rpMetadata.jwks.keys.find(
-      (key) => key.use === "enc",
-    );
-    if (!rpEncKey) {
-      this.log.error("No encryption key found in RP Metadata JWKS");
-      throw new Error("No encryption key found in RP Metadata JWKS");
-    }
-
-    const rpSigKey = options.rpMetadata.jwks.keys.find(
-      (key) => key.use === "sig",
-    );
-    if (!rpSigKey) {
-      this.log.error("No signature key found in RP Metadata JWKS");
-      throw new Error("No signature key found in RP Metadata JWKS");
-    }
-
     const dcqlQuery = requestObject.dcql_query as DcqlQuery | undefined;
     if (!dcqlQuery) {
       throw new Error("dcql_query is missing in the request object");
@@ -245,13 +230,8 @@ export class AuthorizeDefaultStep extends StepFlow {
       },
       iss: options.baseUrl,
       presentationResponseUri: responseUri,
-      signer: {
-        alg: "ES256",
-        method: "jwk" as const,
-        publicJwk: rpSigKey,
-      },
       state: requestObject.state,
-    };
+    } satisfies SendAuthorizationResponseAndExtractCodeOptions;
 
     const authorizeResponse = await sendAuthorizationResponseAndExtractCode(
       sendAuthorizationResponseAndExtractCodeOptions,
